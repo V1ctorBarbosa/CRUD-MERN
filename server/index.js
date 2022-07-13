d2170bb82410df14ff8d9045698be77729505fe6
Victor Barbosa <vicfer2708@gmail.com>
2022-07-13 16:40:01 -0300

    crud mern

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+const foodModel = require('./models/food')
+
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://VictorBarbosa:HnuxaDB8cPWnc$KI5VdD@merncrud.ietem.mongodb.net/food?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+})
+
+app.get('/', async (req,res) => {
+    const food = new foodModel({foodName: "Banana", daysSinceIAte: 3})
+
+    try {
+        await food.save()
+        res.send("inserted data")
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+
+app.listen(3001, () => {
+    console.log("Good to go at 3001")
+})
